dece63e379a84259a738c0257276d063d8a0e9d4
Antonio Santiago <tddmaspid@hotmail.com>
2020-04-07 20:48:45 +0200

    primer paso mostrar datos desglosados con evento hover

---
+++ scripts/Events.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let countries = document.querySelectorAll(".columna");
+    let popup = document.querySelector(".popup");
+    console.log(countries);
+    for (let i = 0; i < countries.length; i++) {
+        countries[i].addEventListener("mouseenter", function  (event) {
+            countries[i].querySelector(".popup").classList.add("popup-hover");
+            
+         });
+        countries[i].addEventListener("mouseleave", function  (event) {
+            countries[i].querySelector(".popup").classList.remove("popup-hover");
+         })
+
+    }
+})
